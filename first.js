16db8fba08a294d2fafcf55c01f0473061963b61
Rasoft <danil.taran1999@gmail.com>
2018-02-19 22:50:16 +0300

    Next
    Interaction between columns made

--- first.js
+++ first.js
@@ -4,6 +4,43 @@ $(function(){
     var template=$(".template").html();
     var $productList= $(".table-left");
 
+    var rightTemplate = $(".rightTemplate").html();
+    var $rightUnbought = $(".whatsleft");
+    var $rightBought=$(".whatssold");
+
+
+    function refreshRight(){
+
+
+        $rightUnbought.html("");
+        $rightBought.html("");
+
+        function addToRight($from,$to){
+
+            $from.each(function(i,item){
+                var $item = $(item);
+                var rightProduct = $(rightTemplate);
+                var amount = $item.find(".number").text();
+                var name = $item.find(".name").text();
+                rightProduct.find(".vertic").text(name);
+                rightProduct.find(".vertic.red").text(amount);
+
+                $to.append(rightProduct);
+
+            });
+
+        }
+
+        addToRight($(".table-left .product.unbought"),$rightUnbought);
+        addToRight($(".table-left .product.bought"),$rightBought);
+
+
+
+
+    }
+
+
+
     function addProduct(name){
 
         var $product =$(template);
@@ -17,14 +54,24 @@ $(function(){
         $product.find(".buyProduct").click(function(){
         $product.removeClass("unbought");
         $product.addClass("bought");
+            refreshRight();
         });
 
         $product.find(".notBought").click(function(){
             $product.removeClass("bought");
             $product.addClass("unbought");
+            refreshRight();
         });
 
+        $product.find(".delete").click(function(){
+            $product.remove();
+            refreshRight();
+        });
+
+
+
         $productList.append($product);
+        refreshRight();
     }
 
 
